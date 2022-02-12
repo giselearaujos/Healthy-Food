0eccc4797a898918aa9c127f061ddcbf010e35c5
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-12 17:01:23 -0300

    :sparkles: Add Button component

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Button from '../Button/index';
 
 const Header = () => {
   return (
-    <div className="header__container">
+    <header className="header__container">
       <h1 className="header__container__title">Healty Food</h1>
       <ul className="header__container__nav">
         <li className="header__container__nav__li">HEALTHY RECIPES</li>
         <li className="header__container__nav__li">BLOG</li>
         <li className="header__container__nav__li">JOIN</li>
+        <Button text="REGISTER" />
       </ul>
-    </div>
+    </header>
   )
 };
 
