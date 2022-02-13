cd4d42c8520405e5280a33427a94f5be4273eb60
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-13 00:29:34 -0300

    :sparkles: Create MainCardFood component

---
+++ src/Components/MainCardFood/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Title from '../Title/index';
+import Subtitle from '../Subtitle/index';
+
+const MainCardFood = () => {
+    return(
+        <div className="section">
+            <Title title="Our Best Recipes"/>
+            <Subtitle
+             subtitle="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+            />
+
+        </div>
+
+    );
+}
+
+export default MainCardFood;
