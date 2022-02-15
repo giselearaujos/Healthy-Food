4678591f72595bd45a0670b45ed290be4a1851fb
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-15 09:51:05 -0300

    :sparkles: Create Register component

---
+++ src/Components/Register/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className="register">
+            <h1 className="register__h1">Cadastre-se</h1>
+            
+        </div>
+    );
+}
+
+export default Register;
