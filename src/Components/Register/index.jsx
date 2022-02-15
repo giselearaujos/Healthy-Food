a7c74d5a455fbff345fc1d445539f989675314dc
giselearaujos <giselearaujo07.s@gmail.com>
2022-02-15 11:02:21 -0300

    :lipstick: Add styles register component

--- src/Components/Register/index.jsx
+++ src/Components/Register/index.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import Title from '../Title/index';
+import Button from '../Button/index';
 
 const Register = () => {
     return (
         <div className="register">
-            <h1 className="register__h1">Cadastre-se</h1>
-            
+            <Title title="Register"/>
+            <form className="register__form">
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="Name"
+                    />
+                </div> 
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="CPF"
+                    />
+                </div> 
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="Street"
+                    />
+                </div> 
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="Number"
+                    />
+                </div> 
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="District"
+                    />
+                </div>  
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="City"
+                    />
+                </div>   
+                <div className="register__form__field">                   
+                    <input type="text"
+                     className="register__form__field__input" 
+                     placeholder="State"
+                    />
+                </div>      
+                
+                <Button text="Submit"/>
+            </form>
         </div>
     );
 }
